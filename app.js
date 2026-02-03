16df37b9a5c045d2a5ab52f5971681466b537956
1one <95573204+shleeshlee@users.noreply.github.com>
2026-02-04 02:49:20 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4580,7 +4580,7 @@ async function checkNewEmails() {
     try {
         const res = await apiRequest('/emails/refresh', { 
             method: 'POST',
-            body: JSON.stringify({ since: pollingStartTime })
+            body: JSON.stringify({})
         });
         if (res.ok) {
             const data = await res.json();
